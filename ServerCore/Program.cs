bb8edce94efedcd8af593041960620c2ecc7f666
형빈 최 <chlgudqls123@naver.com>
2023-06-24 15:38:51 +0900

    test

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -9,31 +9,10 @@ namespace ServerCore
 {
     class Program
     {
-        // 스레드는 각자의 스택을 가지고있으나 전역으로 선언된 스택을 돌려쓸수있다
-        static bool _stop = false;
-
-        static void ThreadMain()
-        {
-            Console.WriteLine("쓰레드 시작");
-            while (!_stop)
-            {
-
-            }
-            Console.WriteLine("쓰레드 종료");
-        }
+        
         static void Main(string[] args)
         {
-            Task t = new Task(ThreadMain);
-            t.Start();
-
-            Thread.Sleep(1000);
-
-            _stop = true;
-
-            Console.WriteLine("Stop 호출");
-            Console.WriteLine("종료 대기중");
-            t.Waitf
-            Console.WriteLine("종료 성공");
+            
 
         }
     }
