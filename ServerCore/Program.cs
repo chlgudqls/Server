d71dc1510b89feefcfe1bd1e92be22f3ec1ce76a
형빈 최 <chlgudqls123@thirautech.com>
2023-06-24 14:27:08 +0900

    일감이나 직원의 제한을 두고 직원이 일감을 초과해도 다음일감을 실행할수있게 하기도하고 스레드로 일을시키고 스레드가 많아도 코어가 부족하면 안되니 스레드관리를 잘해야된다

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ServerCore
 {
@@ -15,7 +16,21 @@ namespace ServerCore
         }
         static void Main(string[] args)
         {
-            // Thread가 정직원이면 ThreadPool은 단기알바
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(5, 5);
+            // 먹통의 단점극복 일감을 지정
+            for (int i = 0; i < 5; i++)
+            {
+                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning); // 오래걸릴것이다 추가해주면 5명제한두고 5명모두일시켜도? 아래 일을 해줌
+                t.Start();
+            }
+            // 최소1명 최대5명을 고용해서 사용하고 
+            // 아마도 최대직원이5명   5명모두가 다돌고있으면 아래의 스레드가 실행되지못한다 직원제한 뒀기때문
+           
+            //for (int i = 0; i < 5; i++) // 5명을 돌린거임
+            //    ThreadPool.QueueUserWorkItem(obj => { while (true) { } });
+
+            // Thread가 정직원이면 ThreadPool은 단기알바  백그라운도로돌아가는 스레드
             ThreadPool.QueueUserWorkItem(MainThread);
             //// 실행될 메인함수를 넣어줘야된다
             //Thread t = new Thread(MainThread);
@@ -28,6 +43,10 @@ namespace ServerCore
             //t.Join();
 
             //Console.WriteLine("Hello World!");
+            while (true)
+            {
+
+            }
         }
     }
 }
