806495a420bb2ad2674d8e93d0e65837bdf48ebd
형빈 최 <chlgudqls123@thirautech.com>
2023-06-24 14:55:23 +0900

    stat

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -15,12 +15,18 @@ namespace ServerCore
         static void ThreadMain()
         {
             Console.WriteLine("쓰레드 시작");
+            while (!_stop)
+            {
 
+            }
             Console.WriteLine("쓰레드 종료");
         }
         static void Main(string[] args)
         {
-            
+            Task t = new Task(ThreadMain);
+            t.Start();
+
+            Thread.Sleep(1000);
         }
     }
 }
