2d7800fffb295bd8e89dd61e9dfb9c0819bc1b6b
형빈 최 <chlgudqls123@naver.com>
2023-06-24 15:36:25 +0900

    test

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -10,7 +10,8 @@ namespace ServerCore
     class Program
     {
         // 스레드는 각자의 스택을 가지고있으나 전역으로 선언된 스택을 돌려쓸수있다
-        static bool _stop = false;
+        // volatile 최적화를 하지말아달라
+        volatile static bool _stop = false;
 
         static void ThreadMain()
         {
@@ -21,6 +22,8 @@ namespace ServerCore
             }
             Console.WriteLine("쓰레드 종료");
         }
+
+        // 어쩃든 릴리즈모드를 사용하면 최적화를 하게되는데 컴퓨터가알아서 이상해보이는코드를 고치기때문에 무한루프에 빠질수가있다
         static void Main(string[] args)
         {
             Task t = new Task(ThreadMain);
@@ -32,7 +35,8 @@ namespace ServerCore
 
             Console.WriteLine("Stop 호출");
             Console.WriteLine("종료 대기중");
-            t.Waitf
+            // 스레드의 Join과 같은 의미 끝날때까지 기다렸다가 실행한다 위의 start가
+            t.Wait();
             Console.WriteLine("종료 성공");
 
         }
