ca9fd89234fe5b5a8c7a77a0ceb0e19ba7c54878
형빈 최 <chlgudqls123@naver.com>
2023-06-27 20:27:20 +0900

    Lock

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -9,33 +9,55 @@ namespace ServerCore
 {
     class Program
     {
-        
+        // 1.근성
+        // 2.양보
+        // 3.갑질
+
+        // 상호배제
+        static object _lock = new object();
+        static SpinLock _lock2 = new SpinLock();
+        static Mutex _lock3 = new Mutex();
+        static ReaderWriterLockSlim _lock4 = new ReaderWriterLockSlim(); // 우선순위가있음 평소에는 좀넓은공용화장실 vip오면 한명만사용
+
+        class Reward
+        {
+
+        }
+
+        static Reward GetReadById(int id)
+        {
+            _lock4.EnterReadLock();
+
+            _lock4.ExitReadLock();
+            return null;
+        }
+
+        static void AddReWard(Reward reward)
+        {
+            _lock4.EnterWriteLock();
+
+            _lock4.ExitWriteLock();
+        }
         static void Main(string[] args)
         {
-            int[,] arr = new int[10000, 10000];
 
+            lock (_lock)
             {
-                long now = DateTime.Now.Ticks;
-                for (int y = 0; y < 10000; y++)
-                {
-                    for (int x = 0; x < 10000; x++)
-                        arr[y, x] = 1;
-                }
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"(y,x) 순서 걸린 시간 {end - now}");
-            }
-
-            {
-                long now = DateTime.Now.Ticks;
-                for (int y = 0; y < 10000; y++)
-                {
-                    for (int x = 0; x < 10000; x++)
-                        arr[x, y] = 1;
-                }
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"(x,y) 순서 걸린 시간 {end - now}");
 
             }
+            bool lockTaken = false;
+
+            try
+            {
+                _lock2.Enter(ref lockTaken);
+            }
+            finally
+            {
+                if(lockTaken)
+                    _lock2.Exit();
+            }
+            // 정상처리가 안됐을 경우대비 bool에 결과물을넣어주는방법으로 처리하기위함
+
         }
     }
 }
