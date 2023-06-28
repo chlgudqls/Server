d20c49f96d8bd7acd827aad1277afeb72628c6d3
형빈 최 <chlgudqls123@naver.com>
2023-06-28 20:08:34 +0900

    s

---
+++ ServerCore/Lock.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ServerCore
+{
+    class Lock
+    {
+        const int EMPTY_FLAG = 0x00000000;
+        const int WRITE_MASK = 0x7FFF0000;
+        const int READ_MASK = 0x0000FFFF;
+        const int MAX_SPIN_COUNT = 5000;
+        
+        int _flag = EMPTY_FLAG;
+
+        public void WriteLock()
+        {
+            int desired = (Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK;
+            while (true)
+            {
+                for (int i = 0; i < MAX_SPIN_COUNT; i++)
+                {
+                    if (Interlocked.CompareExchange(ref _flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
+                        return;
+                    //if (_flag == EMPTY_FLAG)
+                    //    _flag = desired;
+                }
+                Thread.Yield();
+            }
+        }
+
+        public void WriteUnlock()
+        {
+            Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+        }
+
+        public void ReadLock()
+        {
+            while (true)
+            {
+                for (int i = 0; i < MAX_SPIN_COUNT; i++)
+                {
+                    int expected = (_flag & READ_MASK);
+                    if (Interlocked.CompareExchange(ref _flag, expected + 1, expected) == expected)
+                        return;       
+                }
+
+                // 일정 수만큼 돈 후에 양보한다
+                Thread.Yield();
+            }
+        }
+
+        public void ReadUnlock()
+        {
+            Interlocked.Decrement(ref _flag);
+        }
+    }
+}
